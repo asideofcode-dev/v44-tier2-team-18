4f6c2c036204bb83a99b358b1920a92bc8fac371
asideofcode <kumbirai@asideofcode.com>
2023-05-25 10:34:19 +0100

    Refactor implementation of BotMoving

--- src/components/Bot/BotMoving.test.tsx
+++ src/components/Bot/BotMoving.test.tsx
@@ -51,5 +51,3 @@ describe('botMoving function', () => {
     });
 
 });
-
-
--- src/components/Bot/BotMoving.tsx
+++ src/components/Bot/BotMoving.tsx
@@ -1,3 +1,5 @@
+type Position = number[];
+
 // Enum representing the directions
 export enum Direction {
   North = 'North',
@@ -6,75 +8,54 @@ export enum Direction {
   West = 'West'
 }
 
+// Function to change to a random direction excluding the provided direction
+const changeRandomDirection = (exclude: Direction): Direction => {
+  // Get an array of directions excluding the provided direction
+  const directions = Object.values(Direction).filter(dir => dir !== exclude);
 
-export const botMoving = (direction: Direction,
-    position: number[]) => {
-    switch (direction) {
-        case 'North': {
-            if (position[1] === 0) {
-                position[1] += 1;
-                const randomDirection = Math.floor(Math.random() * 3);
-                if (randomDirection === 0) {
-                    direction = 'East';
-                } else if (randomDirection === 1) {
-                    direction = 'West';
-                } else {
-                    direction = 'South';
-                };
-            } else {
-                position[1] -= 1;
-            };
-            break;
-        }
-        case 'South': {
-            if (position[1] === 7) {
-                position[1] -= 1;
-                const randomDirection = Math.floor(Math.random() * 3);
-                if (randomDirection === 0) {
-                    direction = 'East';
-                } else if (randomDirection === 1) {
-                    direction = 'West';
-                } else {
-                    direction = 'North';
-                };
-            } else {
-                position[1] += 1;
-            };
-            break;
-        }
-        case 'East': {
-            if (position[0] === 7) {
-                position[0] -= 1;
-                const randomDirection = Math.floor(Math.random() * 3);
-                if (randomDirection === 0) {
-                    direction = 'South';
-                } else if (randomDirection === 1) {
-                    direction = 'North';
-                } else {
-                    direction = 'West';
-                };
-            } else {
-                position[0] += 1;
-            };
-            break;
-        }
-        case 'West': {
-            if (position[0] === 0) {
-                position[0] += 1;
-                const randomDirection = Math.floor(Math.random() * 3);
-                if (randomDirection === 0) {
-                    direction = 'South';
-                } else if (randomDirection === 1) {
-                    direction = 'North';
-                } else {
-                    direction = 'East';
-                };
-            } else {
-                position[0] -= 1;
-            };
-            break;
-        }
-    }
-    return { direction, position };
+  // Randomly select a direction from the available directions
+  return directions[Math.floor(Math.random() * directions.length)];
 }
 
+// Object mapping each direction to the change in position
+const directionToPositionChange: Record<Direction, [number, number]> = {
+  [Direction.North]: [0, -1],
+  [Direction.South]: [0, 1],
+  [Direction.East]: [1, 0],
+  [Direction.West]: [-1, 0],
+}
+
+// Function to constrain a number within a specified range
+const constrainToBounds = (n: number, min: number, max: number): number =>
+  Math.min(Math.max(n, min), max);
+
+// Main function for moving the bot
+export const botMoving = (
+  direction: Direction,
+  position: Position
+): { direction: Direction; position: Position } => {
+  // Create a copy of the position array to avoid modifying the original array
+  position = [...position];
+
+  // Get the change in position based on the provided direction
+  const change = directionToPositionChange[direction];
+
+  // Update the position based on the change
+  position[0] += change[0];
+  position[1] += change[1];
+
+  // Constrain the position values to ensure they stay within the bounds [0, 7]
+  position[0] = constrainToBounds(position[0], 0, 7);
+  position[1] = constrainToBounds(position[1], 0, 7);
+
+  // Check if the bot is at the boundary
+  const atBoundary = position[0] === 0 || position[0] === 7 || position[1] === 0 || position[1] === 7;
+
+  // If the bot is at the boundary, change to a random direction
+  if (atBoundary) {
+    direction = changeRandomDirection(direction);
+  }
+
+  // Return the updated direction and position
+  return { direction, position };
+}
